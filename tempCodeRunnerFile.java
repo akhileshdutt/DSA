4d1e5d0a3dbf0beaf1be139c6df4511fd2d1c242
akhileshdutt <akhilesh.05.dutt@gmail.com>
2025-04-17 11:58:35 +0530

    two_arr

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+        // for(int i=0; i<arr1.length; i++){
+        //     arr1[i] = in.nextInt();
+        // }
+        // for(int i=0; i<arr1.length; i++){
+        //     System.out.print(arr1[i]+" ");
+        // }
+        // for(int num : arr1){
+        //     System.out.print(num+"-");  
+        // }
+
+        // System.out.print(Arrays.toString(arr1));
+    
