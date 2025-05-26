7926db0aefe32ed7ab09ebd8279a7429151eae30
Akhilesh Dutt <akhilesh.05.dutt@gmail.com>
2025-05-26 22:36:45 +0530

    ce

--- question/ceileingOfNumber.java
+++ question/ceileingOfNumber.java
@@ -1,24 +1,28 @@
 public class ceileingOfNumber {
     public static void main(String[] args) {
-        
+        int[] arr= {2,3,5,8,13,15,25,36,45};
+        int k = search(arr, 10);
+        System.out.print(k);
     }
     static int search(int[] arr, int target){
+        if(target>arr[arr.length]){
+            return -1;
+        }
+
         int start = 0;
         int end = arr.length-1;
 
-        while(start>=end){
+        while(start<=end){
             int mid = start + (end-start)/2;
-            if(mid==target){
-                return arr[mid];
-            }
-            if(mid<target){
+            if(arr[mid]>target){
+                end =mid-1;
+            }else if(target>arr[mid]){
                 start = mid+1;
             }else{
-                end=mid-1;
+                return mid;
             }
+            
         }
-
-
-        return -1;
+        return arr[start];
     }
 }
