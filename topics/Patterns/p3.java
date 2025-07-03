27631add2dfa70270a4f15dd8cc72fb82c49fc74
Akhilesh Dutt <akhilesh.05.dutt@gmail.com>
2025-07-03 17:58:23 +0530

    patt

--- topics/Patterns/p3.java
+++ topics/Patterns/p3.java
@@ -23,11 +23,11 @@ public class p3 {
             }
             System.out.println();
         }
-        for(int i=0; i<=4; i++){
-            for(int j=0; j<=i;j++){
-                System.out.print("@");
+        for(int i=3; i>=0; i--){
+            for(int j=4; j>i;j--){
+                System.out.print(" ");
             }
-            for(int n=0; n<2*i; n++){
+            for(int n=0; n<=i*2+1; n++){
                 System.out.print("*");
             }
             System.out.println();
