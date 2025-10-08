5d1b90a07d023c13f5c9ef6d31d3bc873e71e6c7
Akhilesh Dutt <akhilesh.05.dutt@gmail.com>
2025-10-08 13:41:22 +0530

    vectors

---
+++ OOP/collection/VectorExample.java
@@ -0,0 +1,13 @@
+package collection;
+
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        List<Integer> vector = new Vector<>();
+        vector.add(32);
+        vector.add(27);
+        vector.add(23);
+        System.out.println(vector);                         // vector is exactly same as ArrayList but the main difference is that vector is synchronized. 
+    }
+}
