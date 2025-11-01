403641031d833159d5dcf48148439e85c93d0239
Akhilesh Dutt <akhilesh.05.dutt@gmail.com>
2025-11-01 20:51:28 +0530

    addingNode

--- topics/Linkedlist/LL.java
+++ topics/Linkedlist/LL.java
@@ -9,6 +9,37 @@ public class LL {
         this.size = 0;
     }
 
+    public void first(int value){
+        Node node = new Node(value);
+    }
+
+    public void insertFirst(int value){           // O(1)
+        Node node = new Node(value);              // create a new node
+        node.next = head;                         // point new node's next to current head   
+        head = node;                              // update head to new node
+
+        if(tail == null){                         // if the list was empty, update tail to new node
+            tail = head;
+        }
+        size += 1;
+    }
+    
+    public void insertLast(int value){      // O(1)
+        Node node = new Node(value);    // create a new node
+        tail.next = node;
+        tail = node;
+        size += 1;                      // Increment size
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
     private class Node{
         private int value;
         private Node next;
--- topics/Linkedlist/main.java
+++ topics/Linkedlist/main.java
@@ -3,6 +3,11 @@ package Linkedlist;
 public class main {
     public static void main(String[] args) {
         LL list = new LL();
-    } 
-    
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(2);
+        list.insertFirst(6);
+        list.insertLast(100);
+        list.display();
+    }
 }
