097ebc75f09916929859bcb3683e56a2e094ae83
Akhilesh Dutt <akhilesh.05.dutt@gmail.com>
2026-01-21 01:47:26 +0530

    bs

--- topicWiseQuestions/search.java
+++ topicWiseQuestions/search.java
@@ -1,9 +1,41 @@
 package topicWiseQuestions;
-
+import java.util.Scanner;
 
 public class search{
     public static void main(String[] args) {
-        int[] arr= {2,3,6,5,3,4};
-        
-    }
+        int[] arr= {10,20,30,40,50,60,70,80,90};
+        int n= arr.length;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+
+        // linear search
+
+        // for(int i=0; i<n; i++){
+        //     if(num==arr[i]){
+        //         System.out.println(i+"th position");
+        //     }
+        // }
+
+        int k = BS(arr, num);
+        System.out.println(k+"th position");
+
+        }
+        public static int BS(int[] arr, int target){
+            int n = arr.length;
+            int left = 0;
+            int right = n-1;
+            while(left<right){
+                int mid = left + (right-left)/2;
+                if(arr[mid]==target){
+                    return mid;
+                }
+                else if(arr[mid]<target){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+            return -1;
+        }
 }
